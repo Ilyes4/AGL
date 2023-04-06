76cceb66439467b786a401f353ed35f4458986ab
user <user@DESKTOP-UQ1LV0D>
2023-04-06 21:11:22 +0200

    First commit

---
+++ mavenproject2/src/main/java/Article.java
@@ -0,0 +1,22 @@
+public class Article {
+
+    private categorie Categorie;
+
+    private int Id;
+
+    private string Nom;
+
+    private Double Prix;
+
+    public void getCategorie() {
+    }
+
+    public void getNom() {
+    }
+
+    public void getPrix() {
+    }
+
+    public void getId() {
+    }
+}
---
+++ mavenproject2/src/main/java/Catégorie.java
@@ -0,0 +1,15 @@
+public class Catégorie {
+
+    private string Description;
+
+    private string Nom;
+
+    public void getDescription() {
+    }
+
+    public void getNom() {
+    }
+
+    public void untitledMethod() {
+    }
+}
